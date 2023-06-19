46659c532e3390d8cc4009da14ceb0b3caa1a03f
AustinScrivener <23154717-AustinScrivener@users.noreply.replit.com>
2023-06-19 23:43:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Workbook pages 37 - 54.
+# Copy your code into the various snake_vx files as you
+# reach those check points.
+# Edit the code that is below as needed.
+
+
+import snake_v1
---
+++ snake_v1.py
@@ -0,0 +1,50 @@
+import pygame,time
+
+screen_width = 700
+screen_hight = 500
+grid_size = 20
+
+clock = pygame.time.Clock()
+quit_game = False
+snake_x = screen_width / 2
+snake_y = screen_hight / 2
+snake_x_change = 0
+snake_y_change = 0
+
+pygame.init()
+
+screen = pygame.display.set_mode((screen_width,screen_hight),pygame.RESIZABLE)
+print(pygame.display.get_window_size())
+
+while not quit_game == True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      quit_game = True
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        snake_x_change = -1 * grid_size
+        snake_y_change = 0
+      elif event.key == pygame.K_RIGHT:
+        snake_x_change = grid_size
+        snake_y_change = 0
+      elif event.key == pygame.K_UP:
+        snake_y_change = -1 * grid_size
+        snake_x_change = 0
+      elif event.key == pygame.K_DOWN:
+        snake_y_change = grid_size
+        snake_x_change = 0
+
+  if snake_x >= screen_width or snake_x < 0 or snake_y >= screen_hight or snake_y < 0:
+    quit_game = True
+  
+  snake_x += snake_x_change
+  snake_y += snake_y_change
+  
+  screen.fill('dark green')
+  
+  pygame.draw.rect(screen, 'dark red', [snake_x, snake_y, grid_size, grid_size])
+  
+  pygame.display.update()
+  clock.tick(10)
+
+pygame.quit()
