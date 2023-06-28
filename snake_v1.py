25139cae705ffab8624442f2392af59348dc6306
scriveausti <86276068+scriveausti@users.noreply.github.com>
2023-06-28 22:48:01 +0000

    4

--- snake_v1.py
+++ snake_v1.py
@@ -2,7 +2,9 @@ import pygame,time
 
 screen_width = 700
 screen_hight = 500
-grid_size = 5
+grid_size = 20
+frame_rate = 10
+
 
 clock = pygame.time.Clock()
 quit_game = False
@@ -12,22 +14,43 @@ snake_y = (screen_hight - grid_size) / 2
 snake_x_change = 0
 snake_y_change = 0
 
-def get_middle(screen_hight,screen_width):
-  middle_of_screen = ()
-  return middle_of_screen
 
+'format colour : rgb value (tulip)'
+
+colours = {
+  'black':  (0,0,0),
+  'white':  (255,255,255),
+  'green':  (0,255,0),
+  'red':    (255,0,0),
+  'blue' :  (0,0,255)
+}
+
+def get_middle_screen():
+  '''
+  This it the middle of the output screen 
+  outputs a tulip (middle x, middle y)
+  '''
+  screen_width,screen_hight = pygame.display.get_window_size()
+  print(screen_width,screen_hight)
+  middle_of_screen = (screen_width/2,screen_hight/2)
+  
+  return middle_of_screen
 
 pygame.init()
 
 font = pygame.font.SysFont("arialblack", 50)
 
 def message(msg, text_colour, bkgd_colour):
+  '''
+  displays a text box in the middle of the screen 
+  '''
   txt = font.render(msg, True, text_colour, bkgd_colour,)
-  text_box = txt.get_rect(center = (500, 360))
+  center_of_screen = get_middle_screen()
+  text_box = txt.get_rect(center = center_of_screen)
   screen.blit(txt, text_box)
 
 screen = pygame.display.set_mode((screen_width,screen_hight),pygame.RESIZABLE)
-print(pygame.display.get_window_size())
+print(pygame.display.get_window_size(),screen_width,screen_hight)
 
 while not quit_game == True:
   for event in pygame.event.get():
@@ -58,10 +81,11 @@ while not quit_game == True:
   pygame.draw.rect(screen, 'dark red', [snake_x, snake_y, grid_size, grid_size])
   
   pygame.display.update()
-  clock.tick(30)
+  clock.tick(frame_rate)
 
 message ("You died!", 'dark green', "dark red")
 pygame.display.update()
 time.sleep(3)
 
 pygame.quit()
+quit()
