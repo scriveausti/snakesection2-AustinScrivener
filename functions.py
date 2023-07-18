cfed296ff17722d32c60a239c006c17efe3c3b6a
scriveausti <86276068+scriveausti@users.noreply.github.com>
2023-07-18 23:02:24 +0000

    8 (tail and tail colistion)

--- functions.py
+++ functions.py
@@ -3,15 +3,13 @@ import random as ran
 from colours import colours
 
 def snake_(snake_x,snake_y):
-  ''''''
+  '''
+  
+  '''
   snake_x = 0
   snake_y = 0
   snake_pos_been = {}
   
-  for i in range(0,6):
-    snake_been = snake_x,snake_y
-    snake_pos_been[1] = (snake_been)
-    print (snake_pos_been)
 
 
 
@@ -36,14 +34,14 @@ def get_middle_screen():
 
 
 def draw_snake(screen,
-               colours,
+               colour,
                snake_x,
                snake_y,
                grid_size: int):
     """
   draws the head of the snake
   """
-    pygame.draw.rect(screen, colours['red'], [snake_x, snake_y, grid_size, grid_size])
+    pygame.draw.rect(screen, colours[colour], [snake_x, snake_y, grid_size, grid_size])
 
 
 def snake_tail(score: int):
@@ -53,21 +51,25 @@ def snake_tail(score: int):
     snake_length = score
 
 def generate_fruit(screen_height, screen_width, grid_size):
-    new_fruit_x = (ran.randint(0, round(screen_width / grid_size))) * grid_size
-    new_fruit_y = (ran.randint(0, round(screen_height / grid_size))) * grid_size
-    return new_fruit_x, new_fruit_y
+  """
+  generates a random poin on screen that on the same grid as the snake 
+  """
+  
+  new_fruit_x = (ran.randint(0, round(((screen_width-(grid_size*2)) / grid_size))) * grid_size)+grid_size
+  new_fruit_y = (ran.randint(0, round(((screen_height-(grid_size*2)) / grid_size))) * grid_size)+grid_size
+  return new_fruit_x, new_fruit_y
 
 
-def spawn_fruit(screen, colour: str, fruit_spawned, fruit_x, fruit_y, screen_height, screen_width, grid_size):
+def spawn_fruit(screen, colour: str, fruit_spawned, fruit_x, fruit_y, screen_height, screen_width, grid_size,fruit_size):
     """
-
+    draws the fruit and if there is no fruit on screen it will generate a new one 
     """
     if fruit_spawned == False:
         new_fruit_x, new_fruit_y = generate_fruit(screen_height, screen_width, grid_size)
         print(fruit_x,fruit_y)
         spawned = True
-        pygame.draw.rect(screen, colours[colour], [new_fruit_x, new_fruit_y, grid_size, grid_size])
+        pygame.draw.rect(screen, colours[colour], [new_fruit_x, new_fruit_y, fruit_size, fruit_size])
         return new_fruit_x, new_fruit_y, spawned
     else:
-        pygame.draw.rect(screen, colours[colour], [fruit_x, fruit_y, grid_size, grid_size])
+        pygame.draw.rect(screen, colours[colour], [fruit_x, fruit_y, fruit_size, fruit_size])
         return fruit_x, fruit_y, fruit_spawned
--- snake_v1.py
+++ snake_v1.py
@@ -1,66 +1,93 @@
 import random
-
 import pygame, time
 import functions as fun
 from colours import colours
 
+
+
+snake_colour = 'red'
+fruit_colour = 'orange'
+
+frame_rate = 6
 screen_width = 700
 screen_height = 500
+
 grid_size = 20
-frame_rate = 5
+
 middle_of_screen = ((screen_width / 2), (screen_height / 2))
 score = 0
-
-collection_size = 10
+highscore = 0
 
 clock = pygame.time.Clock()
-quit_game = False
+playing = True
 snake_x = (screen_width - grid_size) / 2
 snake_y = (screen_height - grid_size) / 2
 snake_x_change = 0
 snake_y_change = 0
+
+snake_pos_been = []
+
 fruit_spawned = False
 fruit_x = 0 
 fruit_y = 0
+fruit_size = 20
+fruit_collection_size = (fruit_size/2)
+
+tail_pos_x = {}
+tail_pos_y = {}
+
+background_colour = colours['green']
+
+score_and_highscore_space_from_border = 10
+
+
 
 def message(msg, text_colour, bkgd_colour):
     '''
     displays a text box in the middle of the screen
     '''
-    txt = font.render(msg, True, text_colour, bkgd_colour, )
+    txt = font.render(msg, True, text_colour, bkgd_colour)
     center_of_screen = fun.get_middle_screen()
     text_box = txt.get_rect(center=center_of_screen)
     screen.blit(txt, text_box)
 
-def menu():
-  """"""
+def ui(score,highscore,space_from_border):
+  """
+  displays score and highscore
+  
+  """
+  
+  text_colour = colours['black']
+  bkgd_colour = None
+  #draw score 
+  txt = font.render(('score: {}'.format(score)), True, text_colour, bkgd_colour)
+  text_box = txt.get_rect(topleft= (space_from_border,space_from_border))
+  screen.blit(txt, text_box)
 
-
-
-
-
-
-
-
+  #draw highscore
+  txt = font.render(('highscore: {}'.format(highscore)), True, text_colour, bkgd_colour)
+  screen_width, screen_height = fun.get_screen_size()
+  text_box = txt.get_rect(topright=((screen_width - space_from_border),space_from_border))
+  screen.blit(txt, text_box)
 
 # game start
 pygame.init()
 
-font = pygame.font.SysFont("arialblack", 50)
+font = pygame.font.SysFont("comicsans", 50)
 
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
 print(pygame.display.get_window_size(), screen_width, screen_height)
 
 # main game loop
 
-while not quit_game == True:
+while playing:
     for event in pygame.event.get():
         # when the window is resized the screen width and height are updated
         if event.type == pygame.WINDOWRESIZED:
             screen_width, screen_height = fun.get_screen_size()
 
         if event.type == pygame.QUIT:
-            quit_game = True
+            playing = False
 
         # snake movement
         if event.type == pygame.KEYDOWN:
@@ -81,33 +108,64 @@ while not quit_game == True:
                 snake_y_change = grid_size
                 snake_x_change = 0
 
-    if snake_x >= screen_width or snake_x < 0 or snake_y >= screen_height or snake_y < 0:
-        quit_game = True
 
+  
+    if snake_x >= screen_width - grid_size or snake_x < 0 or snake_y >= screen_height - grid_size or snake_y < 0:
+        playing = False
+
+
+    #update snake position
     snake_x += snake_x_change
     snake_y += snake_y_change
+  
+    #clears the past frame 
+    screen.fill(background_colour)
+    #draws head of snake
+    fun.draw_snake(screen,snake_colour,snake_x,snake_y,grid_size)
+    
 
 
-    screen.fill(colours['green'])
-    fun.draw_snake(screen,colours,snake_x,snake_y,grid_size)
-
-
-
-  #spawn fruit 
-    fruit_x,fruit_y,fruit_spawned = fun.spawn_fruit(screen,"orange",fruit_spawned,fruit_x,fruit_y,screen_height,screen_width,grid_size)
+    #spawn fruit 
+    fruit_x,fruit_y,fruit_spawned = fun.spawn_fruit(screen,fruit_colour,fruit_spawned,fruit_x,fruit_y,screen_height,screen_width,grid_size,fruit_size)
   
 
+
     #fruit collection 
-    if (snake_x <= (fruit_x + collection_size) and snake_y <= (fruit_y + collection_size)) and (snake_x >= (fruit_x - collection_size) and snake_y >= (fruit_y - collection_size)) :
+    if (snake_x <= (fruit_x + fruit_collection_size) and snake_y <= (fruit_y + fruit_collection_size)) and (snake_x >= (fruit_x - fruit_collection_size) and snake_y >= (fruit_y - fruit_collection_size)) :
         score += 1
         print(score)
         fruit_spawned = False
 
+    #snakes history 
+    snake_pos_been.insert(0,snake_y)
+    snake_pos_been.insert(0,snake_x)
+    #clears unused things to make list shorter 
+    if len(snake_pos_been) > (score * 2)+6:
+      snake_pos_been.pop((score * 2)+5)
+      snake_pos_been.pop((score * 2)+6)
 
+
+
+  #draws snakes tail
+    i =2
+    while i < ((score*2)+2):
+      tail_x = snake_pos_been[i]
+      tail_y = snake_pos_been[i+1]
+      tail_pos_x[int((i-2)/2)] = (tail_x)
+      tail_pos_y[int((i-2)/2)] = (tail_y)
+      pygame.draw.rect(screen, colours['dark red'], [tail_x, tail_y, grid_size, grid_size])
+      i += 2
+
+      if snake_y == tail_y and snake_x == tail_x:
+        playing = False
+  
+
+    ui(score, highscore ,score_and_highscore_space_from_border)
+      
     pygame.display.update()
     clock.tick(frame_rate)
 
-message("You died!", colours['green'], colours['red'])
+message("You died!", colours['red'], None)
 pygame.display.update()
 time.sleep(3)
 
